d439b8649f2a3746c8bc5d80fc94b07d40488ca0
vaingloryss <3275691399@qq.com>
2019-09-28 17:35:47 +0800

    springMVC听课笔记。

--- src/main/java/com/vainglory/controller/UserController.java
+++ src/main/java/com/vainglory/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -15,7 +16,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/getUserList")
+    @ResponseBody
+    @RequestMapping(value = "/getUserList",produces = "utf-8")
     public String getUserList(Model model){
         List<User> userList = userService.getUserList();
         model.addAttribute("userList",userList);
